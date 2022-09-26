c71aa922bf64b7103a9ed9976ba41bc67baebfe1
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-09-27 00:37:27 +0300

    Adds color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+import getRandomHexColor from './color';
 
+const COLOR_FREQUENCY = 1000;
+
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', onStartClick);
+
+function onStartClick(e) {
+  const { startBtn, stopBtn } = refs;
+  const intervID = setInterval(changeColor, COLOR_FREQUENCY);
+
+  changeColor();
+  toggleActiveBtn(stopBtn, startBtn);
+
+  stopBtn.addEventListener('click', onStopClick(intervID), { once: true });
+}
+
+function onStopClick(intervID) {
+  return function (e) {
+    const { startBtn, stopBtn } = refs;
+
+    clearInterval(intervID);
+    toggleActiveBtn(startBtn, stopBtn);
+  };
+}
+
+function changeColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function toggleActiveBtn(activeBtn, inactiveBtn) {
+  inactiveBtn.setAttribute('disabled', true);
+  activeBtn.removeAttribute('disabled');
+}
